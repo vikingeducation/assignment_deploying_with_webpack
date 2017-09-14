f55aea92cef2fa17df739a0e3d2a28eff43d9e00
Will Timpson <will.timpson@gmail.com>
2017-09-14 14:06:22 -0400

    display some results. yay.

--- components/App.js
+++ components/App.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "elemental";
 
+const Location = ({ distance, latt_long, title, woeid }) => <p>{title}</p>;
+
 const App = ({ coordinates, locations }) => (
-  <Container>
-    <h1 className="greet">Hello, there!</h1>
-    <p>lat: {coordinates.lat}</p>
-    <p>long: {coordinates.long}</p>
-  </Container>
+  <Router>
+    <Container>
+      <h1 className="greet">Hello, there!</h1>
+      <h2>Selected Coordinates</h2>
+      <p>Latitude: {coordinates.lat}</p>
+      <p>Longitude: {coordinates.long}</p>
+
+      {locations.length &&
+        locations.map(location => (
+          <Location key={location.woeid} {...location} />
+        ))}
+    </Container>
+  </Router>
 );
 
 export default App;
