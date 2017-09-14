6d3fee0f06753cd44e1b3daaf8ac960a7e5e19f2
iandavid <iandavid@iandavids-MacBook-Pro.local>
2017-09-14 11:42:10 -0400

    got it to work i guess

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ import ReactDOM from "react-dom";
 
 const Greeting = ({ name }) => <p className="greet">Hello, {name}!</p>;
 
-ReactDOM.render(<Greeting name="Reign" />, document.getElementById("root"));
+ReactDOM.render(<Greeting name="Reign" />, document.getElementById("gerby"));
