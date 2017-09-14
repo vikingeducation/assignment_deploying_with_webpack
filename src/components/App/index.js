63b4744df1fa3e132e9da20aad8f524d6f30a994
Benjamin Soung <soungbenjamin@gmail.com>
2017-09-14 13:07:22 -0400

    working

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,18 +4,32 @@ import axios from "axios";
 export default class App extends Component {
   constructor() {
     super();
-    getLocation();
-    navigator.geolocation.getCurrentPosition(position => {
+    // getLocation();
+    
+  }
+
+  componentDidMount() {
+    const success = function(position) {
+       console.log('hello');
+      console.log(position, 'wtf is this???');
       console.log(
         "lat and long = ",
         position.coords.latitude,
         position.coords.longitude
-      );
-    });
+      )
+    }
+
+    const failure = function(error) {
+      console.error(error);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, failure);
+ 
+
   }
 
   render() {
-    getLocation;
+
     return <div>hello react</div>;
   }
 }
