04aedf7d1eac675df84504a96beca5f920a81c39
Eric Glover <etgt28@mail.missouri.edu>
2017-09-14 12:16:08 -0500

    testing location

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,52 +1,57 @@
 import React, { Component } from "react";
 import axios from "axios";
 
+var triggerGeolocationRequest = function() {
+  var options = {
+    enableHighAccuracy: true,
+    maximumAge: 1000
+  };
+
+  var result;
+
+  const failure = function(error) {
+    console.error(error);
+  };
+
+  window.navigator.geolocation.getCurrentPosition(
+    function(position) {
+      var result = {
+        type: "success",
+        data: { lat: position.coords.latitude, lng: position.coords.longitude }
+      };
+
+      console.log("result ", result);
+    },
+    failure,
+    options
+  );
+};
+
 export default class App extends Component {
   constructor() {
     super();
-    // getLocation();
-    
   }
 
   componentDidMount() {
+    triggerGeolocationRequest();
     const success = function(position) {
-       console.log('hello');
-      console.log(position, 'wtf is this???');
+      console.log("hello");
+      console.log(position, "wtf is this???");
       console.log(
         "lat and long = ",
         position.coords.latitude,
         position.coords.longitude
-      )
-    }
+      );
+    };
 
     const failure = function(error) {
       console.error(error);
-    }
+    };
 
-    navigator.geolocation.getCurrentPosition(success, failure);
- 
-
+    // navigator.geolocation.getCurrentPosition(success, failure);
   }
 
   render() {
-
     return <div>hello react</div>;
   }
 }
-//
-// const getLocation = async () => {
-//   let done = navigator.geolocation.getCurrentPosition(position);
-//   console.log(
-//     "lat and long = ",
-//     position.coords.latitude,
-//     position.coords.longitude
-//   );
-//   return [position.coords.latitude, position.coords.longitude];
-//   // navigator.geolocation.getCurrentPosition(position => {
-//   //   console.log(
-//   //     "lat and long = ",
-//   //     position.coords.latitude,
-//   //     position.coords.longitude
-//   //   );
-//   // });
-// };
