c16a44fed7fe79cb0cc93b7b85a1b2f6deb1a93d
Stephanie Barker <song_mage@yahoo.com>
2017-09-14 13:04:09 -0400

    yay apis are so fun to work with

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-import React from "react";
-import ReactDOM from "react-dom";
+const React = require("react");
+const ReactDOM = require("react-dom");
+const fetch = require("isomorphic-fetch");
 
-const Greeting = ({ name }) => <p className="greet">Hello, {name}!</p>;
+class AppContainer extends React.Component {
+	componentDidMount() {
+		// navigator.geolocation.getCurrentPosition(position => {
+		// 	const { longitude, latitude } = position.coords;
 
-ReactDOM.render(<Greeting name="Reign" />, document.getElementById("gerby"));
+		fetch(
+			`https://www.metaweather.com/api/location/search/?lattlong=41.4532007,-85.7564007`,
+			{ mode: "cors" }
+		)
+			.then(response => {
+				console.log(response);
+				return response.json();
+			})
+			.then(data => {
+				console.log(data);
+			})
+			.catch(err => {
+				console.log(err);
+			});
+		// https://www.metaweather.com/api/location/search/?lattlong=(latt),(long)
+		// /api/location/(woeid)/(date)/
+	}
+	// }
+
+	render() {
+		return <p>Hello, {name}!</p>;
+	}
+}
+
+ReactDOM.render(<AppContainer />, document.getElementById("gerby"));
---
+++ server.js
@@ -0,0 +1,17 @@
+const fetch = require("isomorphic-fetch");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+	fetch(
+		"https://www.metaweather.com/api/location/search/?lattlong=41.4532007,-85.7564007",
+		{ mode: "no-cors" }
+	)
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			return res.json(data);
+		});
+});
+
+app.listen(3001);
